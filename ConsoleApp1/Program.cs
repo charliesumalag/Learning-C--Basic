37c9c8206f84e236d06a1cb1f5e37e4ae79f88a5
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-08-15 11:21:53 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+static void SimpleNoKissAndKiss()
+{
+    Console.WriteLine("NO KISS");
+    int sampleNumber = 100;
+    string numberString = ConvertToString(sampleNumber);
+    Console.WriteLine(sampleNumber);
+
+
+    string ConvertToString(int number)
+    {
+        return number.ToString(); 
+    }
+
+
+    Console.WriteLine("KISS");
+    int sampleNumberWithKISS = 100;
+    Console.WriteLine(sampleNumberWithKISS);
+
+
+}
+
+
+SimpleNoKissAndKiss();
---
+++ Episode1VariableAndDataTypes/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+#region --About Variables --
+// Local Variables/Explicit
+
+int sampleNumber = 3;
+
+
+
+// Implicit
+var sampleNumberImplicit = 3;
+dynamic sampleNumberDynamic = 5;
+
+
+//Local Variables
+
+
+#endregion
+
+#region --Parse and Convert --
+
+string textOne = "1";
+var intOne = int.Parse(textOne);
+var result = intOne * 5;
+
+Console.WriteLine(result);
+
+
+
+#endregion
+public class Episode1Class
+{
+
+    #region --About Variables--
+    int sampleFieldInt = 5; // global variable of the class. can be accessible for all in this class
+    static int sampleStaticInt = 6;
+    public void sampleMethod1()
+    {
+        //this two is local variable. it accessible only here in this method/block of code.
+        int sampleLocalInt = 1;
+        var sampleLocalDouble = 1.5;
+        sampleFieldInt = 300; // global varibale accessed.
+        sampleStaticInt = 22;
+    }
+
+    public void sampleMethod2()
+    {
+        //this two is local variable. it accessible only here in this method/block of code.
+        int sample2Int = 1;
+    }
+
+    #endregion
+}
